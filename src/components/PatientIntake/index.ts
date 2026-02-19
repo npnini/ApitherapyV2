e56aa5f3c571602f07003b8f34b359fcd8d752c9
Nati Perlman <nati.perlman@gmail.com>
2026-02-19 09:44:25 +0200

    Fix App.tsx errors and remove 19 zombie files

--- src/components/PatientIntake/index.ts
+++ src/components/PatientIntake/index.ts
@@ -1,5 +1,2 @@
 export { default as PatientIntake } from './PatientIntake';
-export { default as PatientIntakeNav } from './PatientIntakeNav';
 export { default as PersonalDetails } from './PersonalDetails';
-export type { default as MedicalRecord } from './MedicalRecord';
-export { default as Questionnaire } from './Questionnaire';
